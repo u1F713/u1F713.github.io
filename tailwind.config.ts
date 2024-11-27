68c5372716537e0a03afc474158dd580f4cd0145
Nyarlathotep <anhedonia@skiff.com>
2024-11-27 07:04:45 -0500

    feat(font): use `IBM Plex Mono`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   content: ['./src/**/*.{astro,md,mdx,ts,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
   plugins: []
 } as Config
