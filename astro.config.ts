abd9c94c70f4a9882963e04b1eb8976a9bfe70a6
Nyarlathotep <anhedonia@skiff.com>
2024-11-27 12:43:54 -0500

    feat(deps): add @astrojs/mdx

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 export default defineConfig({
   server: { port: 4443 },
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 })
