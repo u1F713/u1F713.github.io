c248aca65432a584332616f5d9cf25c3d536a050
Nyarlathotep <anhedonia@skiff.com>
2024-12-03 18:48:58 -0500

    refactor(css): dynamic accent color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,14 @@
 import typography from '@tailwindcss/typography'
 import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
+import * as R from 'remeda'
 
-export default {
+const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
+const lightness = [
+  97.78, 93.56, 88.11, 82.67, 74.22, 64.78, 57.33, 46.89, 39.44, 32, 23.78
+]
+
+const config: Config = {
   content: ['./src/**/*.{astro,md,mdx,ts,tsx}'],
   theme: {
     extend: {
@@ -10,20 +16,17 @@ export default {
         sans: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.sans]
       },
 
-      colors: {
-        'color-50': 'oklch(var(--color-50))',
-        'color-100': 'oklch(var(--color-100))',
-        'color-200': 'oklch(var(--color-200))',
-        'color-300': 'oklch(var(--color-300))',
-        'color-400': 'oklch(var(--color-400))',
-        'color-500': 'oklch(var(--color-500))',
-        'color-600': 'oklch(var(--color-600))',
-        'color-700': 'oklch(var(--color-700))',
-        'color-800': 'oklch(var(--color-800))',
-        'color-900': 'oklch(var(--color-900))',
-        'color-950': 'oklch(var(--color-950))'
-      }
+      colors: R.reduce(
+        shades,
+        (obj, shade, index) => ({
+          ...obj,
+          [`dn-accent-${shade}`]: `oklch(${lightness[index]}% var(--dn-accent) 0.29)`
+        }),
+        {}
+      )
     }
   },
   plugins: [typography]
-} as Config
+}
+
+export default config
