c70f74990360622432b21c4ff3481002586c8d35
Nyarlathotep <anhedonia@skiff.com>
2024-12-02 21:42:14 -0500

    feat(css): update theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
+import typography from '@tailwindcss/typography'
 import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
-import typography from '@tailwindcss/typography'
 
 export default {
   content: ['./src/**/*.{astro,md,mdx,ts,tsx}'],
@@ -8,6 +8,20 @@ export default {
     extend: {
       fontFamily: {
         sans: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.sans]
+      },
+
+      colors: {
+        'color-50': 'oklch(var(--color-50))',
+        'color-100': 'oklch(var(--color-100))',
+        'color-200': 'oklch(var(--color-200))',
+        'color-300': 'oklch(var(--color-300))',
+        'color-400': 'oklch(var(--color-400))',
+        'color-500': 'oklch(var(--color-500))',
+        'color-600': 'oklch(var(--color-600))',
+        'color-700': 'oklch(var(--color-700))',
+        'color-800': 'oklch(var(--color-800))',
+        'color-900': 'oklch(var(--color-900))',
+        'color-950': 'oklch(var(--color-950))'
       }
     }
   },
