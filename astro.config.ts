be43f7ae094019c7e5ab4fc97eb46007ab0a25c5
Nyarlathotep <anhedonia@skiff.com>
2024-12-12 17:45:50 -0500

    feat: integrate solidjs

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@tailwindcss/vite'
 import mdx from '@astrojs/mdx'
+import solidJs from '@astrojs/solid-js'
 
 export default defineConfig({
   server: { port: 4443 },
-  integrations: [mdx()],
+  integrations: [mdx(), solidJs()],
   vite: { plugins: [tailwind()] }
 })
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,23 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintPluginAstro from 'eslint-plugin-astro'
+import solid from 'eslint-plugin-solid/configs/typescript'
+import * as tsParser from '@typescript-eslint/parser'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   ...eslintPluginAstro.configs.recommended,
+  {
+    ...solid,
+    files: ['**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: 'tsconfig.json'
+      }
+    }
+  },
   { ignores: ['.astro'] }
 )
