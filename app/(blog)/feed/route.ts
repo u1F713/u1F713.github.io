04b5210274a7abaa000a57576226f4491aaaae26
Nyarlathotep <anhedonia@skiff.com>
2025-01-17 22:47:30 -0500

    feat(web feed): render entry content

--- app/(blog)/feed/route.ts
+++ app/(blog)/feed/route.ts
@@ -1,18 +1,32 @@
+import { render } from '@/lib/markdown-content/render.ts'
 import { NodeContext } from '@effect/platform-node'
-import { Chunk, ManagedRuntime, Order, Stream } from 'effect'
+import { Chunk, Effect, ManagedRuntime, Order, Stream } from 'effect'
 import { ArticleScheme } from '../article-scheme.ts'
 import { getArticles } from '../utils.ts'
 
 export async function GET() {
+  const { renderToStaticMarkup } = await import('react-dom/server')
+
   const runtime = ManagedRuntime.make(NodeContext.layer)
-  const articleChunk = await runtime.runPromise(Stream.runCollect(getArticles))
-  const articles = Chunk.sort(
-    articleChunk,
-    Order.mapInput(
-      Order.reverse(Order.Date),
-      (article: { data: ArticleScheme }) => article.data.pubDate
-    )
-  ).pipe(Chunk.toReadonlyArray)
+  const articleTask = getArticles.pipe(
+    Stream.mapEffect(
+      Effect.fn(function* ({ id, data, content }) {
+        const Content = yield* render(content)
+        return { id, data, content: renderToStaticMarkup(Content({})) }
+      })
+    ),
+    Stream.runCollect,
+    Effect.map(
+      Chunk.sort(
+        Order.mapInput(
+          Order.reverse(Order.Date),
+          (article: { data: ArticleScheme }) => article.data.pubDate
+        )
+      )
+    ),
+    Effect.map(Chunk.toReadonlyArray)
+  )
+  const articles = await runtime.runPromise(articleTask)
 
   return new Response(
     `<?xml version="1.0" encoding="UTF-8"?>
@@ -27,13 +41,16 @@ export async function GET() {
 	   <email>anhedonia@skiff.com</email>
     </author>
     ${articles.reduce(
-      (acc, { id, data }) =>
+      (acc, { id, data, content }) =>
         `${acc}
          <entry>
             <id>${id}</id>
             <title>${data.title}</title>
             <link href="https://u1f713.github.io/${id}"/>
             <updated>${data.updatedDate ?? data.pubDate}</updated>
+            <content type="text/html">
+              ${content}
+            </content>
           </entry>`,
       ''
     )}
