2e0c68f98a479b1fbffe5cf9c85aa9d56a2c6899
Nyarlathotep <anhedonia@skiff.com>
2025-01-19 22:20:18 -0500

    feat: implement image caching

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -1,34 +1,11 @@
-import { Cloudinary } from '@cloudinary/url-gen'
-import { Delivery, Resize } from '@cloudinary/url-gen/actions'
-import { auto } from '@cloudinary/url-gen/qualifiers/format'
-import { Config, Effect, pipe } from 'effect'
+import {
+  cacheImage,
+  cloudinaryLayer,
+  fetchImage,
+  readMetadata
+} from '@/lib/external-image'
+import { Effect } from 'effect'
 import NextImage from 'next/image'
-import sharp from 'sharp'
-
-const getCloudinaryImage = Effect.fn(function* (
-  src: string,
-  width?: number | string,
-  height?: number | string
-) {
-  const url = yield* Config.string('CLOUDINARY_CLOUD_NAME').pipe(
-    Effect.map(cloudName => new Cloudinary({ cloud: { cloudName } })),
-    Effect.map(cloud =>
-      cloud
-        .image(src)
-        .delivery(Delivery.format(auto()))
-        .resize(height ? Resize.scale().height(height) : Resize.auto())
-        .resize(width ? Resize.scale().width(width) : Resize.auto())
-        .toURL()
-    )
-  )
-  const meta = yield* pipe(
-    Effect.promise(() => fetch(url)),
-    Effect.flatMap(res => Effect.promise(() => res.blob())),
-    Effect.flatMap(blob => Effect.promise(() => blob.arrayBuffer())),
-    Effect.flatMap(buffer => Effect.promise(() => sharp(buffer).metadata()))
-  )
-  return { url, ...meta }
-})
 
 type ImageProps = React.ImgHTMLAttributes<HTMLImageElement> & {
   src: string
@@ -37,15 +14,28 @@ type ImageProps = React.ImgHTMLAttributes<HTMLImageElement> & {
   loading?: 'eager' | 'lazy'
 }
 
-async function Image({ src, alt, ...props }: ImageProps) {
-  const { url, width, height } = await Effect.runPromise(
-    getCloudinaryImage(src, props.width, props.height)
+async function Image({ src: publicID, width, height, ...props }: ImageProps) {
+  const getCloudinaryImage = Effect.gen(function* () {
+    const buffer = yield* fetchImage({ publicID, height, width })
+    const metadata = yield* readMetadata(buffer)
+    const url = yield* cacheImage(publicID, new Uint8Array(buffer))
+    return { url, ...metadata }
+  })
+  const image = await Effect.runPromise(
+    getCloudinaryImage.pipe(Effect.provide(cloudinaryLayer), Effect.scoped)
   )
-  if (!width || !height) {
+  if (!image.width || !image.height) {
     throw new Error('Failed to read image metadata')
   }
+
   return (
-    <NextImage {...props} src={url} alt={alt} width={width} height={height} />
+    <NextImage
+      {...props}
+      width={image.width}
+      height={image.height}
+      src={`/${image.url}`}
+    />
   )
 }
+
 export default Image
