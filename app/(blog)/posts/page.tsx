59fa8ccba3f575db23af901ad45d88590c3f8bab
Nyarlathotep <anhedonia@skiff.com>
2025-02-06 15:11:14 -0500

    style: post list

--- app/(blog)/posts/page.tsx
+++ app/(blog)/posts/page.tsx
@@ -10,7 +10,7 @@ import { Chunk, Effect, ManagedRuntime, Order, pipe, Stream } from 'effect'
 import Link from 'next/link'
 import { PostScheme } from '../postScheme.ts'
 
-async function Posts() {
+export default async function Posts() {
   const runtime = ManagedRuntime.make(NodeContext.layer)
 
   const postsOrder = Order.mapInput(
@@ -36,15 +36,15 @@ async function Posts() {
 
   return (
     <div className="border-ds-border mx-auto h-full w-full max-w-screen-xl md:border-x">
-      <div className="flex gap-4 p-4 align-middle">
-        <h1 className="text-xl font-semibold lg:text-2xl">All blog posts</h1>
-      </div>
+      <h1 className="px-4 py-10 text-3xl font-semibold lg:px-6 lg:text-5xl">
+        Blog posts
+      </h1>
 
       <ul className="grid grid-cols-1 grid-rows-1 gap-[1px] md:grid-cols-2 lg:grid-cols-3">
         {Chunk.toArray(posts).map(({ id, data }) => (
           <li key={id}>
             <Link href={`/${id}`} about="">
-              <ArticleCard {...data} />{' '}
+              <ArticleCard {...data} />
             </Link>
           </li>
         ))}
@@ -53,40 +53,36 @@ async function Posts() {
   )
 }
 
-function ArticleCard({ title, description, pubDate, tags }: PostScheme) {
-  const formattedDatePubDate = new Intl.DateTimeFormat(navigator.language, {
-    dateStyle: 'long'
-  }).format(pubDate)
+const ArticleCard = ({ title, description, pubDate, tags }: PostScheme) => (
+  <article
+    className={clsx(
+      'grid h-full min-h-70 grid-cols-1 grid-rows-[auto_1fr] gap-5 break-words lg:min-h-90',
+      'hover:bg-ds-border/40 shadow-ds-border group p-4 shadow-[0_0_0_1px] lg:p-6'
+    )}
+  >
+    <section>
+      <p className="mb-2">
+        <time dateTime={pubDate.toDateString()}>
+          {pubDate.toLocaleString('en-US', { dateStyle: 'long' })}
+        </time>
+      </p>
 
-  return (
-    <article
-      className={clsx(
-        'grid h-full min-h-70 grid-cols-1 grid-rows-[auto_1fr] gap-5 break-words lg:min-h-90',
-        'hover:bg-ds-border/40 shadow-ds-border group p-4 shadow-[0_0_0_1px] lg:p-6'
-      )}
-    >
-      <section>
-        <h3 className="mb-2 text-xl lg:text-3xl">{title}</h3>
-        <span className="text-ds-text/70 mb-4 font-bold">
-          {formattedDatePubDate}
-        </span>
-      </section>
+      <h3 className="text-2xl lg:text-3xl">{title}</h3>
+      <span className="text-ds-text/70 mb-4 font-bold"></span>
+    </section>
 
-      <section>
-        <p className="group-hover:text-ds-text">{description}</p>
-        <ul className="mt-4 flex flex-row-reverse flex-wrap gap-2">
-          {tags?.map(tag => (
-            <li
-              key={tag}
-              className="shadow-ds-border rounded-sm p-2 text-sm shadow-[0_0_0_1px]"
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </section>
-    </article>
-  )
-}
-
-export default Posts
+    <section>
+      <p className="group-hover:text-ds-text">{description}</p>
+      <ul className="mt-4 flex flex-row-reverse flex-wrap gap-2">
+        {tags?.map(tag => (
+          <li
+            key={tag}
+            className="shadow-ds-border rounded-sm p-2 text-sm shadow-[0_0_0_1px]"
+          >
+            {tag}
+          </li>
+        ))}
+      </ul>
+    </section>
+  </article>
+)
