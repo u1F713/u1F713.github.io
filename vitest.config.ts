075a34b611af2d61e606dc1956370ca75ae67bf3
Nyarlathotep <anhedonia@skiff.com>
2025-02-08 08:52:35 -0500

    fix: load local `envFile`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
+import fs from 'node:fs'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
-const { pathname: envFile } = new URL('.env.local', import.meta.url)
-process.loadEnvFile(envFile)
+const envFile = '.env.local'
+if (fs.existsSync(envFile)) process.loadEnvFile(envFile)
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
