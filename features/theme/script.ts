00368f0061230a6b8040e5386a937c88b63887ac
Nyarlathotep <anhedonia@skiff.com>
2025-02-20 21:35:25 -0500

    fix(theme): default to media-prefers when color-scheme is unset

--- features/theme/script.ts
+++ features/theme/script.ts
@@ -2,7 +2,7 @@ export const preloadTheme = (): undefined => {
   const colorScheme = localStorage.getItem('theme-color-scheme')
   document.documentElement.classList.toggle(
     'dark',
-    colorScheme === 'auto'
+    !colorScheme || colorScheme === 'auto'
       ? window.matchMedia('(prefers-color-scheme: dark)').matches
       : colorScheme === 'dark'
   )
